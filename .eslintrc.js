7ae5acfead8a4e15b97fefa49a87042dc1947154
David Leger <dleger@manifold.co>
2019-01-31 14:10:24 -0400

    add more linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,35 @@ module.exports = {
     },
   },
   rules: {
-    'import/prefer-default-export': 'off',
-    'class-methods-use-this': 'off',
+    'arrow-parens': 'off', // let Prettier decide
+    camelcase: 'off', // underscores are a thing
+    'class-methods-use-this': 'off', // component lifecycle methods sometimes don't use `this`
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'never',
+        functions: 'never', // function commas are weird
+        imports: 'never',
+        objects: 'always-multiline',
+      },
+    ],
+    'function-paren-newline': 'off', // let Prettier decide
+    'implicit-arrow-linebreak': 'off', // let Prettier decide
+    'import/prefer-default-export': 'off', // named exports are perfectly fine
+    'max-len': 'off', // let Prettier decide
+    'object-curly-newline': 'off', // let Prettier decide
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        'import/no-unresolved': 'off', // handled by TS
+        indent: 'off', // thrown off by types
+        'no-undef': 'off', // handled by TS
+        'no-unused-vars': 'off', // handled by TS
+      },
+    },
+  ],
 };
