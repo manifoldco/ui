88d6ac89af71729e903409167575e7304ddca945
David Leger <dleger@manifold.co>
2019-02-04 21:06:36 -0400

    added rational order to stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order', // provides a rational order of properites as a base
+  ],
+  plugins: ['stylelint-order'],
   rules: {
+    'declaration-colon-newline-after': null, // this is fine
     'property-no-vendor-prefix': null, // some properties need vendor prefixes
-    'value-list-comma-newline-after': null,
+    'value-list-comma-newline-after': null, // this is fine
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements'],
+      },
+    ],
   },
   syntax: 'postcss',
 };
