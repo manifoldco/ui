4a915c9cead79df63d4f204807b903a36a723ddf
Drew Powers <drew@pow.rs>
2019-02-08 16:28:27 -0700

    Add counting test

--- src/components/service-grid/service-grid.e2e.ts
+++ src/components/service-grid/service-grid.e2e.ts
@@ -3,6 +3,19 @@ import { newE2EPage } from '@stencil/core/testing';
 /* eslint-disable no-param-reassign */
 
 describe('<service-grid>', () => {
+  it('displays all services', async () => {
+    // Set properties and wait
+    const page = await newE2EPage({ html: '<service-grid></service-grid>' });
+    await page.$eval('service-grid', (elm: any) => {
+      elm.services = [{ body: {} }, { body: {} }, { body: {} }];
+    });
+    await page.waitForChanges();
+
+    // See if all service cards are rendered
+    const el = await page.findAll('service-grid >>> service-card');
+    expect(el.length).toBe(3);
+  });
+
   it('formats links correctly', async () => {
     // Set properties and wait
     const page = await newE2EPage({ html: '<service-grid></service-grid>' });
