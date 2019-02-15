a34ab403dcdc27de7d671f2a5d37ef4811b6ff9a
David Leger <dleger@manifold.co>
2019-02-14 20:22:02 -0400

    refactor tests

--- src/components/image-gallery/image-gallery.e2e.ts
+++ src/components/image-gallery/image-gallery.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage, E2EPage } from '@stencil/core/testing';
 
 /* eslint-disable no-param-reassign */
 
@@ -10,8 +10,10 @@ const images = [
 ];
 
 describe('<image-gallery>', () => {
-  it('displays the first image by default', async () => {
-    const page = await newE2EPage({
+  let page: E2EPage;
+
+  beforeAll(async () => {
+    page = await newE2EPage({
       html: `<image-gallery></image-gallery>`,
     });
 
@@ -24,27 +26,15 @@ describe('<image-gallery>', () => {
       ];
     });
     await page.waitForChanges();
-    const el = await page.find('image-gallery >>> img[data-test="display-image"]');
+  });
 
+  it('displays the first image by default', async () => {
+    const el = await page.find('image-gallery >>> img[data-test="display-image"]');
     expect(el.getAttribute('src')).toBe(images[0]);
   });
 
   it('displays the image thumbnails', async () => {
-    const page = await newE2EPage({
-      html: `<image-gallery></image-gallery>`,
-    });
-
-    await page.$eval('image-gallery', (elm: any) => {
-      elm.images = [
-        'https://cdn.manifold.co/providers/jawsdb/screenshots/ss1.PNG',
-        'https://cdn.manifold.co/providers/jawsdb/screenshots/ss2.PNG',
-        'https://cdn.manifold.co/providers/jawsdb/screenshots/ss3.PNG',
-        'https://cdn.manifold.co/providers/jawsdb/screenshots/ss4.PNG',
-      ];
-    });
-    await page.waitForChanges();
     const els = await page.findAll('image-gallery >>> img[data-test="thumbnail"]');
-
     expect(els.length).toBe(4);
   });
 });
--- src/components/link-button/link-button.e2e.ts
+++ src/components/link-button/link-button.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage, E2EPage } from '@stencil/core/testing';
 
 /* eslint-disable no-param-reassign */
 
@@ -6,21 +6,21 @@ const url = 'https://manifold.co';
 const label = 'Greatest Website Ever';
 
 describe('<link-button>', () => {
-  it('links to the provided href', async () => {
-    const page = await newE2EPage({
+  let page: E2EPage;
+
+  beforeAll(async () => {
+    page = await newE2EPage({
       html: `<link-button href="${url}">${label}</link-button>`,
     });
-    const el = await page.find('link-button >>> a');
+  });
 
+  it('links to the provided href', async () => {
+    const el = await page.find('link-button >>> a');
     expect(el.getAttribute('href')).toBe(url);
   });
 
   it('displays the label', async () => {
-    const page = await newE2EPage({
-      html: `<link-button href="${url}">${label}</link-button>`,
-    });
     const el = await page.find('link-button');
-
     expect(el.innerText).toBe(label);
   });
 });
