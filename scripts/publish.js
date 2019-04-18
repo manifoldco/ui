36ded1eefe6e385b16c4aef59186f2c2f3837db5
Drew Powers <drew@pow.rs>
2019-04-17 22:29:04 -0600

    Fix npm script, add README badges

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,50 +1,46 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { existsSync, mkdirSync, readFileSync, writeFileSync } = require('fs');
 const { resolve } = require('path');
 const { execSync } = require('child_process');
+const { copySync } = require('fs-extra');
 const prettier = require('prettier');
 
 /* eslint-disable no-console */
 
 const SELECT_FLAG = /-.*/; // Selects anything after a hyphen, or nothing
-const DIST_DIR = resolve(__dirname, '..', 'dist');
+const oldDistDir = resolve(__dirname, '..', 'dist');
+const newDistDir = resolve(__dirname, '..', 'pkg', 'dist');
+const pkgDir = resolve(__dirname, '..', 'pkg');
 
-// 1. Read Git tag
+// 1. Setup: make dirs, copy README
+if (!existsSync(pkgDir)) mkdirSync(pkgDir);
+copySync(oldDistDir, newDistDir);
+copySync(resolve(__dirname, '..', 'readme.md'), resolve(pkgDir, 'readme.md'));
+
+// 2. Read Git tag
 const version = execSync('git describe --abbrev=0 --tags --exact-match')
   .toString()
   .replace('v', '')
   .replace('\n', '');
 console.log(`📦 Publishing ${version}`);
 
-// 2. Determine if “public” or “private” release
+// 3. Determine if @latest or @[other]
 let npmTag = 'latest'; // default (“public”)
-
-// If no hyphen, this is a “private“ release
-const flag = SELECT_FLAG.exec(version);
+const flag = SELECT_FLAG.exec(version); // If no hyphen, this is a “private“ release
 if (flag && flag[0].length > 1) {
   const flagClean = flag[0].replace('-', '');
   npmTag = flagClean.includes('.') ? flagClean.split('.')[0] : flagClean;
 }
 
-// 3. Generate package.json for npm
+// 4. Copy package.json, and add version
 const packageJSON = JSON.parse(readFileSync(resolve(__dirname, '..', 'package.json'), 'utf8'));
-// These have to be overridden to work on npm, and Stencil complains about these values
-const newPackageJSON = {
-  ...packageJSON,
-  version,
-  files: ['*'],
-  main: './index.js',
-  esnext: './esm',
-  module: './esm',
-  types: './types/components.d.ts',
-  collection: './collection/collection-manifest.json',
-};
+const newPackageJSON = { ...packageJSON, version };
 delete newPackageJSON.scripts; // Don’t need this on npm
 writeFileSync(
-  resolve(DIST_DIR, 'package.json'),
+  resolve(pkgDir, 'package.json'),
   prettier.format(JSON.stringify(newPackageJSON), { parser: 'json' })
 );
 
-// 4. Publish to npm
-execSync(`npm publish ../dist --tag ${npmTag}`, {
-  cwd: __dirname, // Set cwd, so it can find ../dist/
+// 5. Publish to npm
+execSync(`npm publish ../pkg --tag ${npmTag}`, {
+  cwd: __dirname, // Set cwd, so it can find ../pkg/
 });
