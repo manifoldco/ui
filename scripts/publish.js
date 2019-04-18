38c8f23d795d95512655c33af1e0a876b77e80c3
Drew Powers <drew@pow.rs>
2019-04-17 22:46:50 -0600

    Add deploy message

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,6 +6,8 @@ const prettier = require('prettier');
 
 /* eslint-disable no-console */
 
+const timeStart = process.hrtime();
+
 const SELECT_FLAG = /-.*/; // Selects anything after a hyphen, or nothing
 const oldDistDir = resolve(__dirname, '..', 'dist');
 const newDistDir = resolve(__dirname, '..', 'pkg', 'dist');
@@ -16,13 +18,14 @@ if (!existsSync(pkgDir)) mkdirSync(pkgDir);
 emptyDirSync(pkgDir);
 copySync(oldDistDir, newDistDir);
 copySync(resolve(__dirname, '..', 'readme.md'), resolve(pkgDir, 'readme.md'));
+console.log(`[1/3] 🧹 Cleaning up…`);
 
 // 2. Read Git tag
 const version = execSync('git describe --abbrev=0 --tags --exact-match')
   .toString()
   .replace('v', '')
   .replace('\n', '');
-console.log(`📦 Publishing ${version}`);
+console.log(`[2/3] 📦 Publishing ${version} to npm…`);
 
 // 3. Determine if @latest or @[other]
 let npmTag = 'latest'; // default (“public”)
@@ -36,6 +39,7 @@ if (flag && flag[0].length > 1) {
 const packageJSON = JSON.parse(readFileSync(resolve(__dirname, '..', 'package.json'), 'utf8'));
 const newPackageJSON = { ...packageJSON, version };
 delete newPackageJSON.scripts; // Don’t need this on npm
+delete newPackageJSON.files; // Ship all files in pkg/
 writeFileSync(
   resolve(pkgDir, 'package.json'),
   prettier.format(JSON.stringify(newPackageJSON), { parser: 'json' })
@@ -45,3 +49,7 @@ writeFileSync(
 execSync(`npm publish ../pkg --tag ${npmTag}`, {
   cwd: __dirname, // Set cwd, so it can find ../pkg/
 });
+
+const timeEnd = process.hrtime(timeStart);
+const time = timeEnd[0] + Math.round(timeEnd[1] / 100000000) / 10;
+console.log(`[3/3] 🚀 Deployed ${version} in ${time}s`);
