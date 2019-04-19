62add5b14239975b9b33a38bcc4c56c6165f746a
Drew Powers <drew@pow.rs>
2019-04-19 17:35:36 -0600

    Fix icon test

--- src/components/manifold-icon/manifold-icon.e2e.ts
+++ src/components/manifold-icon/manifold-icon.e2e.ts
@@ -1,25 +1,28 @@
 import { newE2EPage } from '@stencil/core/testing';
-import { icon } from '@manifoldco/icons';
+import { activity } from '@manifoldco/icons';
 
 /* eslint-disable no-param-reassign, @typescript-eslint/no-explicit-any */
-
-const iconKey = 'activity';
 
 describe('<manifold-icon>', () => {
   it('displays the icon (gradient)', async () => {
     const page = await newE2EPage({
       html: `<manifold-icon />`,
     });
-    await page.$eval('manifold-icon', (elm: any) => {
-      elm.icon = 'activity';
-      elm.style = '--gradient: linear-gradient(to top right, #329dd1, #4f50a4)';
-      elm.gradient = '--gradient';
-    });
+    const props = { icon: activity };
+    await page.$eval(
+      'manifold-icon',
+      (elm: any, { icon }: any) => {
+        elm.icon = icon;
+        elm.style = '--gradient: linear-gradient(to top right, #329dd1, #4f50a4)';
+        elm.gradient = '--gradient';
+      },
+      props
+    );
 
     await page.waitForChanges();
 
     const el = await page.find('manifold-icon >>> path');
-    expect(el.getAttribute('d')).toBe(icon[iconKey]);
+    expect(el.getAttribute('d')).toBe(activity);
     expect(el.getAttribute('d')).toBeTruthy();
   });
 
@@ -28,15 +31,20 @@ describe('<manifold-icon>', () => {
       html: `<manifold-icon />`,
     });
 
-    await page.$eval('manifold-icon', (elm: any) => {
-      elm.icon = 'activity';
-      elm.color = '#888888';
-    });
+    const props = { icon: activity };
+    await page.$eval(
+      'manifold-icon',
+      (elm: any, { icon }: any) => {
+        elm.icon = icon;
+        elm.color = '#888888';
+      },
+      props
+    );
 
     await page.waitForChanges();
 
     const el = await page.find('manifold-icon >>> path');
-    expect(el.getAttribute('d')).toBe(icon[iconKey]);
+    expect(el.getAttribute('d')).toBe(activity);
     expect(el.getAttribute('d')).toBeTruthy();
   });
 });
