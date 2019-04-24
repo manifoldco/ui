0f01d3bfaa061efc5556c26d7ea747e2094222da
Sam Slotsky <sam@manifold.co>
2019-04-24 08:46:29 -0500

    Happo working on dev

---
+++ .happo.js
@@ -0,0 +1,16 @@
+const { RemoteBrowserTarget } = require('happo.io');
+const path = require('path');
+
+module.exports = {
+  project: 'manifoldco/ui',
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+  targets: {
+    chrome: new RemoteBrowserTarget('chrome', {
+      viewport: '1024x768',
+    }),
+  },
+  type: 'plain',
+  prerender: false,
+  setupScript: path.resolve(__dirname, 'happoSetup.js'),
+};
---
+++ src/components/manifold-select/manifold-select-happo.jsx
@@ -0,0 +1,26 @@
+import { defineCustomElements } from '../../../dist/loader';
+
+defineCustomElements(window);
+
+export const primary = render => {
+  const defaultValue = 'foo';
+  const options = [
+    {
+      label: 'Foo',
+      value: 'foo',
+    },
+    {
+      label: 'Bar',
+      value: 'bar',
+    },
+  ];
+
+  const name = 'Fubar';
+
+  const select = document.createElement('manifold-select');
+  select.defaultValue = defaultValue;
+  select.options = options;
+  select.name = name;
+
+  document.body.appendChild(select);
+};
