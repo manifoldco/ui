cd7bfc226df8510867f889a00fd0bb3d237de612
Drew Powers <dangodev@users.noreply.github.com>
2019-04-25 16:32:25 -0600

    Add billing endpoint (#98)

--- src/utils/connections.ts
+++ src/utils/connections.ts
@@ -1,4 +1,5 @@
 export interface Connection {
+  billing: string;
   catalog: string;
   gateway: string;
   marketplace: string;
@@ -9,11 +10,13 @@ type Connections = { [K in Env]: Connection };
 
 export const connections: Connections = {
   stage: {
+    billing: 'https://api.billing.stage.manifold.co/v1',
     catalog: 'https://api.catalog.stage.manifold.co/v1',
     gateway: 'https://api.stage.manifold.co/v1',
     marketplace: 'https://api.marketplace.stage.manifold.co/v1',
   },
   prod: {
+    billing: 'https://api.billing.manifold.co/v1',
     catalog: 'https://api.catalog.manifold.co/v1',
     gateway: 'https://api.manifold.co/v1',
     marketplace: 'https://api.marketplace.manifold.co/v1',
