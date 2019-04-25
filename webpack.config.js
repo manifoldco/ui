d47fe2608effb6102acefe8b57b61c66e39f13ff
Chloe Edeal <chloeedeal@Chloes-MacBook-Pro.local>
2019-04-25 13:45:08 -0600

    configuring ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './app.ts',
+  output: {
+    filename: 'bundle.js',
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+};
