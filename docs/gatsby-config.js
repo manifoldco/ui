83494850969b0557d4cbfacebf4e5dbf70b6c6cd
Drew Powers <drew@pow.rs>
2019-05-10 11:19:43 -0600

    Add Styled Components plugin for Gatsby

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-typescript-checker`,
     {
