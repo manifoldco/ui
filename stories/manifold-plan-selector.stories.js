c8de8136d2617a605cb15f9ef9bacf327c12f74b
Drew Powers <drew@pow.rs>
2019-05-16 09:43:29 -0600

    Add more key products

--- stories/manifold-plan-selector.stories.js
+++ stories/manifold-plan-selector.stories.js
@@ -3,6 +3,8 @@ import markdown from '../docs/docs/components/manifold-plan-selector.md';
 
 storiesOf('Plan Selector', module)
   .addParameters({ readme: { sidebar: markdown } })
+  .add('LogDNA', () => '<manifold-plan-selector product-label="logdna"></manifold-plan-selector>')
+  .add('Mailgun', () => '<manifold-plan-selector product-label="mailgun"></manifold-plan-selector>')
   .add(
     'JawsDB',
     () => '<manifold-plan-selector product-label="jawsdb-mysql"></manifold-plan-selector>'
@@ -11,7 +13,5 @@ storiesOf('Plan Selector', module)
     'Memcachier',
     () => '<manifold-plan-selector product-label="memcachier-cache"></manifold-plan-selector>'
   )
-  .add(
-    'Zerosix',
-    () => '<manifold-plan-selector product-label="zerosix"></manifold-plan-selector>'
-  );
+  .add('Zerosix', () => '<manifold-plan-selector product-label="zerosix"></manifold-plan-selector>')
+  .add('Ziggeo', () => '<manifold-plan-selector product-label="ziggeo"></manifold-plan-selector>');
