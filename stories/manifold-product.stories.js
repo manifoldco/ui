ba993957a97e1489157afb8cdb1730d53ef0c949
Drew Powers <drew@pow.rs>
2019-05-17 11:50:49 -0600

    Add button to story, simplify CSS

--- stories/manifold-product.stories.js
+++ stories/manifold-product.stories.js
@@ -5,7 +5,15 @@ storiesOf('Product', module)
   .addParameters({ readme: { sidebar: markdown } })
   .add(
     'JawsDB',
-    () =>
-      '<manifold-product product-label="jawsdb-mysql"><manifold-link-button slot="cta">Get JawsDB MySQL</manifold-link-button></manifold-product>'
+    () => `
+      <manifold-product product-label="jawsdb-mysql">
+        <manifold-link-button slot="cta">Get JawsDB MySQL</manifold-link-button>
+      </manifold-product>`
   )
-  .add('Zerosix', () => '<manifold-product product-label="zerosix"></manifold-product>');
+  .add(
+    'Zerosix',
+    () => `
+      <manifold-product product-label="zerosix">
+        <manifold-link-button slot="cta">Get Zerosix</manifold-link-button>
+      </manifold-product>`
+  );
