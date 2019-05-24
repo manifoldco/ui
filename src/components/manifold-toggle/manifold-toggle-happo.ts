534e29b7b7bd61e8a1b7c4768810ec1a3aaf4fd5
Chloe Edeal <chloeedeal@Chloes-MacBook-Pro.local>
2019-05-24 16:02:05 -0600

    Added more toggle tests

--- src/components/manifold-toggle/manifold-toggle-happo.ts
+++ src/components/manifold-toggle/manifold-toggle-happo.ts
@@ -3,3 +3,24 @@ export const primary = () => {
 
   document.body.appendChild(toggle);
 };
+
+export const labeled = () => {
+  const toggle = document.createElement('manifold-toggle');
+  toggle.label = 'Happo Test Label';
+
+  document.body.appendChild(toggle);
+};
+
+export const disabled = () => {
+  const toggle = document.createElement('manifold-toggle');
+  toggle.disabled = true;
+
+  document.body.appendChild(toggle);
+};
+
+export const checked = () => {
+  const toggle = document.createElement('manifold-toggle');
+  toggle.defaultValue = true;
+
+  document.body.appendChild(toggle);
+};
