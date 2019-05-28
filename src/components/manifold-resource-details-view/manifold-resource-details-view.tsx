94d584e0648df370b636e38dff7e4fe399e5a732
Sam Slotsky <sslotsky@users.noreply.github.com>
2019-05-28 07:40:00 -0500

    VRT for resource details (#142)

---
+++ src/components/manifold-resource-details-view/manifold-resource-details-view.tsx
@@ -0,0 +1,86 @@
+import { h, Component, Prop } from '@stencil/core';
+
+import { FeatureName } from './components/FeatureName';
+import { FeatureValue } from './components/FeatureValue';
+import { $ } from '../../utils/currency';
+
+@Component({
+  tag: 'manifold-resource-details-view',
+  styleUrl: 'style.css',
+  shadow: true,
+})
+export class ManifoldResourceDetails {
+  @Prop() data?: Gateway.Resource;
+
+  render() {
+    if (this.data) {
+      const { expanded_features, name } = this.data.plan as Gateway.ResolvedPlan;
+      const { estimated_cost, features: customFeatures = [] } = this.data;
+      return (
+        <div class="container">
+          <h3 class="heading">Plan Features</h3>
+          <div class="details">
+            {estimated_cost && [
+              <span class="amount">{$(estimated_cost.cost)}</span>,
+              <span class="suffix">/mo</span>,
+            ]}
+            <p class="plan-name">{name}</p>
+          </div>
+          <dl class="features">
+            {expanded_features.map(feature => {
+              const customFeature = customFeatures.find(({ label }) => label === feature.label);
+              const customValue = customFeature && customFeature.value.value;
+
+              return [
+                <dt class="feature-name">
+                  <FeatureName feature={feature} />
+                </dt>,
+                <dd class="feature-value">
+                  <FeatureValue feature={feature} value={customValue} />
+                </dd>,
+              ];
+            })}
+          </dl>
+        </div>
+      );
+    }
+
+    // 💀
+    return (
+      <div class="container">
+        <h3 class="heading">Plan Features</h3>
+        <div class="details">
+          <span class="amount">
+            <manifold-skeleton-text>0.00/mo</manifold-skeleton-text>
+          </span>
+          <p class="plan-name">
+            <manifold-skeleton-text>Plan Name</manifold-skeleton-text>
+          </p>
+        </div>
+        <dl class="features">
+          <dt class="feature-name">
+            <manifold-skeleton-text>Feature Name</manifold-skeleton-text>
+          </dt>
+          <dd class="feature-value">
+            <manifold-skeleton-text>Feature Value</manifold-skeleton-text>
+          </dd>
+
+          <dt class="feature-name">
+            <manifold-skeleton-text>
+              The greatest and best feature in the world
+            </manifold-skeleton-text>
+          </dt>
+          <dd class="feature-value">
+            <manifold-skeleton-text>Tribute</manifold-skeleton-text>
+          </dd>
+          <dt class="feature-name">
+            <manifold-skeleton-text>Another Feature Name</manifold-skeleton-text>
+          </dt>
+          <dd class="feature-value">
+            <manifold-skeleton-text>Another Feature Value</manifold-skeleton-text>
+          </dd>
+        </dl>
+      </div>
+    );
+  }
+}
--- src/components/manifold-resource-details/manifold-resource-details.tsx
+++ src/components/manifold-resource-details/manifold-resource-details.tsx
@@ -1,113 +1,14 @@
-import { h, Component, Prop, Element, State, Watch } from '@stencil/core';
+import { h, Component } from '@stencil/core';
 
-import Tunnel from '../../data/connection';
-import { withAuth } from '../../utils/auth';
-import { Connection, connections } from '../../utils/connections';
+import ResourceTunnel, { ResourceState } from '../../data/resource';
 
-import { FeatureName } from './components/FeatureName';
-import { FeatureValue } from './components/FeatureValue';
-import { $ } from '../../utils/currency';
-
-@Component({
-  tag: 'manifold-resource-details',
-  styleUrl: 'style.css',
-  shadow: true,
-})
+@Component({ tag: 'manifold-resource-details' })
 export class ManifoldResourceDetails {
-  @Element() el: HTMLElement;
-  /** _(hidden)_ Passed by `<manifold-connection>` */
-  @Prop() connection: Connection = connections.prod;
-  /** Which resource does this belong to? */
-  @Prop() resourceName: string;
-  @State() resource?: Gateway.Resource;
-  @Watch('resourceName') resourceChange(newName: string) {
-    this.resource = undefined;
-    this.fetchResource(newName);
-  }
-
-  fetchResource(resourceName: string) {
-    return fetch(`${this.connection.gateway}/resources/me/${resourceName}`, withAuth())
-      .then(response => response.json())
-      .then((resource: Gateway.Resource) => {
-        this.resource = resource;
-      });
-  }
-
-  async componentWillLoad() {
-    return this.fetchResource(this.resourceName);
-  }
-
   render() {
-    if (this.resource) {
-      const { expanded_features, name } = this.resource.plan as Gateway.ResolvedPlan;
-      const { estimated_cost, features: customFeatures = [] } = this.resource;
-      return (
-        <div class="container">
-          <h3 class="heading">Plan Features</h3>
-          <div class="details">
-            {estimated_cost && [
-              <span class="amount">{$(estimated_cost.cost)}</span>,
-              <span class="suffix">/mo</span>,
-            ]}
-            <p class="plan-name">{name}</p>
-          </div>
-          <dl class="features">
-            {expanded_features.map(feature => {
-              const customFeature = customFeatures.find(({ label }) => label === feature.label);
-              const customValue = customFeature && customFeature.value.value;
-
-              return [
-                <dt class="feature-name">
-                  <FeatureName feature={feature} />
-                </dt>,
-                <dd class="feature-value">
-                  <FeatureValue feature={feature} value={customValue} />
-                </dd>,
-              ];
-            })}
-          </dl>
-        </div>
-      );
-    }
-
-    // 💀
     return (
-      <div class="container">
-        <h3 class="heading">Plan Features</h3>
-        <div class="details">
-          <span class="amount">
-            <manifold-skeleton-text>0.00/mo</manifold-skeleton-text>
-          </span>
-          <p class="plan-name">
-            <manifold-skeleton-text>Plan Name</manifold-skeleton-text>
-          </p>
-        </div>
-        <dl class="features">
-          <dt class="feature-name">
-            <manifold-skeleton-text>Feature Name</manifold-skeleton-text>
-          </dt>
-          <dd class="feature-value">
-            <manifold-skeleton-text>Feature Value</manifold-skeleton-text>
-          </dd>
-
-          <dt class="feature-name">
-            <manifold-skeleton-text>
-              The greatest and best feature in the world
-            </manifold-skeleton-text>
-          </dt>
-          <dd class="feature-value">
-            <manifold-skeleton-text>Tribute</manifold-skeleton-text>
-          </dd>
-          <dt class="feature-name">
-            <manifold-skeleton-text>Another Feature Name</manifold-skeleton-text>
-          </dt>
-          <dd class="feature-value">
-            <manifold-skeleton-text>Another Feature Value</manifold-skeleton-text>
-          </dd>
-        </dl>
-      </div>
+      <ResourceTunnel.Consumer>
+        {(state: ResourceState) => <manifold-resource-details-view data={state.data} />}
+      </ResourceTunnel.Consumer>
     );
   }
 }
-
-Tunnel.injectProps(ManifoldResourceDetails, ['connection']);
