6c96137f0c1ceedcb1b6cd5f583291eb0d7bd334
Sam Slotsky <sam@manifold.co>
2019-05-28 14:54:58 -0500

    Fix attribute casing

--- src/components/manifold-tooltip/manifold-tooltip-happo.tsx
+++ src/components/manifold-tooltip/manifold-tooltip-happo.tsx
@@ -4,9 +4,9 @@ import toHTML from '../../../test-utils/to-html';
 
 export const primary = () => {
   const content = `
-    <manifold-tooltip labelText="This is a tooltip">
+    <manifold-tooltip label-text="This is a tooltip">
       <span class="value" data-value="42" data-locked>
-        <manifold-icon class="icon" icon="${lock}" marginRight></manifold-icon>
+        <manifold-icon class="icon" icon="${lock}" margin-right></manifold-icon>
         42
       </span>
   `;
