9775e6e4120e497b2d24454f667078b2c348d84d
Chloe Edeal <chloeedeal@Chloes-MacBook-Pro.local>
2019-05-29 11:40:43 -0600

    Adding manifold-button slot to product page test

--- src/components/manifold-product-page/manifold-product-page-happo.ts
+++ src/components/manifold-product-page/manifold-product-page-happo.ts
@@ -1,5 +1,7 @@
 import jawsdbMock from '../../spec/mock/jawsdb/product.json';
+import jawsdbProvider from '../../spec/mock/jawsdb/provider.json';
 import fromJSON from '../../spec/mock/fromJSON';
+import toHTML from '../../../test-utils/to-html';
 
 export const skeleton = () => {
   const productPage = document.createElement('manifold-product-page');
@@ -8,8 +10,19 @@ export const skeleton = () => {
 };
 
 export const jawsdb = () => {
-  const productPage = document.createElement('manifold-product-page');
-  productPage.product = fromJSON(jawsdbMock);
+  // const productPage = document.createElement('manifold-product-page');
+  // productPage.product = fromJSON(jawsdbMock);
+  // productPage.provider = fromJSON(jawsdbProvider);
+
+    const content = `
+      <manifold-product-page product="${fromJSON(jawsdbMock)}" provider="${fromJSON(jawsdbProvider)}">
+        <div class="sidebar-cta">
+          <manifold-button name="cta" />
+        </div>
+      </manifold-product-page>
+    `;
+  
+    const productPage = toHTML(content) as HTMLManifoldProductPageElement;
 
   document.body.appendChild(productPage);
 };
