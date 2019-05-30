7d96bcbe98d7fb127ce6715162f192a5b9c062d2
Chloe Edeal <chloeedeal@Chloes-MacBook-Pro.local>
2019-05-30 11:09:02 -0600

    Added provider and slot to product page

--- src/components/manifold-product-page/manifold-product-page-happo.ts
+++ src/components/manifold-product-page/manifold-product-page-happo.ts
@@ -1,7 +1,7 @@
 import jawsdbMock from '../../spec/mock/jawsdb/product.json';
 import jawsdbProvider from '../../spec/mock/jawsdb/provider.json';
+
 import fromJSON from '../../spec/mock/fromJSON';
-import toHTML from '../../../test-utils/to-html';
 
 export const skeleton = () => {
   const productPage = document.createElement('manifold-product-page');
@@ -9,20 +9,17 @@ export const skeleton = () => {
   document.body.appendChild(productPage);
 };
 
-export const jawsdb = () => {
-  // const productPage = document.createElement('manifold-product-page');
-  // productPage.product = fromJSON(jawsdbMock);
-  // productPage.provider = fromJSON(jawsdbProvider);
+export const jawsDB = () => {
+  const productPage = document.createElement('manifold-product-page');
+  productPage.product = fromJSON(jawsdbMock);
+  productPage.provider = fromJSON(jawsdbProvider);
 
-    const content = `
-      <manifold-product-page product="${fromJSON(jawsdbMock)}" provider="${fromJSON(jawsdbProvider)}">
-        <div class="sidebar-cta">
-          <manifold-button name="cta" />
-        </div>
-      </manifold-product-page>
-    `;
-  
-    const productPage = toHTML(content) as HTMLManifoldProductPageElement;
+  const button = document.createElement('manifold-button');
+  button.textContent = 'Get JawsDB MySQL';
+  button.slot = 'cta';
 
+  productPage.appendChild(button);
   document.body.appendChild(productPage);
+
+  return productPage.componentOnReady();
 };
