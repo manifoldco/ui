27ad6359a9bca327633cd8bda2deef662e981b07
David Leger <dleger@manifold.co>
2019-07-12 12:40:13 -0300

    get that token workin'

--- src/components/manifold-auth-token/manifold-auth-token.tsx
+++ src/components/manifold-auth-token/manifold-auth-token.tsx
@@ -1,10 +1,9 @@
-import { h, Component, Prop, Element, Watch } from '@stencil/core';
-import { defineCustomElements } from '@manifoldco/shadowcat/dist/loader';
+import { h, Component, Prop, Watch } from '@stencil/core';
+// import '@manifoldco/shadowcat';
 import Tunnel from '../../data/connection';
 
 @Component({ tag: 'manifold-auth-token' })
 export class ManifoldAuthToken {
-  @Element() el: HTMLElement;
   /** _(hidden)_ Passed by `<manifold-connection>` */
   @Prop() setAuthToken?: (s: string) => void;
   /* Authorisation header token that can be used to authenticate the user in manifold */
@@ -15,45 +14,14 @@ export class ManifoldAuthToken {
   }
 
   componentWillLoad() {
-    defineCustomElements(window);
-
     if (this.setAuthToken && this.token) {
       this.setAuthToken(this.token);
     }
   }
-
-  componentDidLoad() {
-    function listener(el: Element | null) {
-      return new Promise<string>(resolve => {
-        if (el) {
-          el.addEventListener('receiveManifoldToken', (e: CustomEvent) => {
-            console.log('token received from iframe: ', e.detail);
-            resolve(e.detail);
-          });
-        }
-      });
-    }
-
-    async function setToken(el: Element | null) {
-      const newToken: string = await listener(el);
-      console.log('newToken:', newToken);
-      return newToken;
-    }
-
-    const oauth = document.querySelector('#test');
-    const newToken = setToken(oauth);
-
-    // this is setting the token to be a Promise<string> and I don't understand
-    // how to turn it into a string in the setToken() function
-    if (this.token) {
-      this.token = newToken;
-    }
-  }
-
-  componentWillUpdate() {
-    if (this.token) {
-      console.log('TOKEN WILL UPDATE: ', this.token);
-    }
+  async componentDidLoad() {
+    document.addEventListener('receiveManifoldToken', (e: CustomEvent) => {
+      this.token = e.detail;
+    });
   }
 
   render() {
