a783edc856a1c4de8b1b241b0c66a5ec49043f0a
Chloe Edeal <caedeal@gmail.com>
2019-07-16 15:56:32 -0600

    Converting to graphql fetch and adding tunnel

--- src/components/manifold-product-page/manifold-product-page.tsx
+++ src/components/manifold-product-page/manifold-product-page.tsx
@@ -1,8 +1,31 @@
-import { h, Component, Prop } from '@stencil/core';
+import { h, Component, Prop, Watch } from '@stencil/core';
+import { gql } from '@manifoldco/gql-zero';
 import { arrow_up_right, book, life_buoy } from '@manifoldco/icons';
-import { Catalog } from '../../types/catalog';
+import graphqlFetch from '../../utils/graphqlFetch';
+import { Product, Provider } from '../../types/graphql'; 
 import skeletonProduct from '../../data/product';
+import Tunnel from '../../data/connection';
+import { withAuth } from '../../utils/auth';
 import { categoryIcon } from '../../utils/marketplace';
+import { Connection, connections } from '../../utils/connections';
+
+const query = gql`
+  query PRODUCT_PAGE($productId: String!) {
+    product(id: $productId) {
+      displayName
+      documentationUrl
+      supportEmail
+      label
+      logoUrl
+      categories {
+        label
+      }
+      provider {
+        displayName
+      }
+    }
+  }
+`;
 
 @Component({
   tag: 'manifold-product-page',
@@ -10,21 +33,48 @@ import { categoryIcon } from '../../utils/marketplace';
   shadow: true,
 })
 export class ManifoldProductPage {
-  @Prop() product?: Catalog.Product;
-  @Prop() provider?: Catalog.Provider;
+  @Prop() product?: Product;
+  @Prop() productId?: string;
+  @Prop() provider?: Provider;
+  @Prop() connection?: Connection = connections.prod;
+  @Prop() authToken?: string;
+  @Watch('productId') productChange(newProduct: string) {
+    this.fetchProdProv(newProduct);
+  }
 
-  get providerName() {
-    if (!this.product || !this.provider) {
-      return undefined;
+  //provider name
+  //product documentation_url, support_url, name, label, logo_url, tags
+
+  // get providerName() {
+  //   if (!this.product || !this.provider) return undefined;
+  //   return (
+  //     (this.provider.body.name !== this.product.body.name && this.provider.body.name) || undefined
+  //   );
+  // }
+
+  componentWillLoad() {
+    if (this.productId) this.fetchProdProv(this.productId);
+  }
+
+  fetchProdProv = async (productId: string) => {
+    if (!this.connection) {
+      return;
     }
-    return (
-      (this.provider.body.name !== this.product.body.name && this.provider.body.name) || undefined
-    );
+
+    this.product = undefined;
+    this.provider = undefined;
+    const variables = { productId };
+    const { data, error } = await graphqlFetch({ query, variables });
+    if (error) {
+      console.error(error);
+    }
+    this.product = data.product;
+    this.provider = data.product.provider;
   }
 
   render() {
     if (this.product) {
-      const { documentation_url, support_email, name, label, logo_url, tags } = this.product.body;
+      const { documentationUrl, supportEmail, displayName, label, logoUrl, categories } = this.product;
       const gradient = `var(--manifold-g-${label}, var(--manifold-g-default))`;
 
       return (
@@ -34,31 +84,31 @@ export class ManifoldProductPage {
               <div class="sidebar-inner">
                 <div class="sidebar-card" style={{ '--background-gradient': gradient }}>
                   <div class="product-logo">
-                    <img src={logo_url} alt={name} itemprop="logo" />
+                    <img src={logoUrl} alt={displayName} itemprop="logo" />
                   </div>
                   <h2 class="product-name" itemprop="name">
-                    {name}
+                    {displayName}
                   </h2>
                   <p class="provider-name">
-                    {this.providerName && <span itemprop="brand">from {this.providerName}</span>}
+                    {this.provider.displayName && <span itemprop="brand">from {this.provider.displayName}</span>}
                   </p>
                 </div>
                 <div class="sidebar-cta">
                   <slot name="cta" />
                 </div>
-                {tags && (
+                {categories && (
                   <div class="sidebar-section">
                     <h4>Category</h4>
-                    {tags.map(tag => (
+                    {categories.map(category => (
                       <div
                         class="category"
-                        style={{ '--categoryColor': `var(--manifold-c-${tag})` }}
+                        style={{ '--categoryColor': `var(--manifold-c-${category})` }}
                       >
                         <manifold-icon
-                          icon={categoryIcon[tag] || categoryIcon.uncategorized}
+                          icon={categoryIcon[category.label] || categoryIcon.uncategorized}
                           margin-right
                         />
-                        {tag}
+                        {category}
                       </div>
                     ))}
                   </div>
@@ -66,14 +116,14 @@ export class ManifoldProductPage {
                 <div class="sidebar-section">
                   <h4>Links</h4>
                   <div class="provider-link">
-                    <a href={documentation_url} target="_blank" rel="noopener noreferrer">
+                    <a href={documentationUrl} target="_blank" rel="noopener noreferrer">
                       <manifold-icon icon={book} margin-right />
                       Docs
                       <manifold-icon class="external-link-icon" icon={arrow_up_right} margin-left />
                     </a>
                   </div>
                   <div class="provider-link">
-                    <a href={`mailto:${support_email}`} target="_blank" rel="noopener noreferrer">
+                    <a href={`mailto:${supportEmail}`} target="_blank" rel="noopener noreferrer">
                       <manifold-icon icon={life_buoy} margin-right />
                       Support
                       <manifold-icon class="external-link-icon" icon={arrow_up_right} margin-left />
@@ -123,3 +173,5 @@ export class ManifoldProductPage {
     );
   }
 }
+
+Tunnel.injectProps(ManifoldProductPage, ['connection', 'authToken']);
