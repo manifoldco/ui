8f04c1e5383bc04862f1b1b3f0206367af715de9
Sam Slotsky <sam@manifold.co>
2019-07-19 13:17:31 -0500

    Add expired variant to token tests

--- src/components/manifold-auth-token/manifold-auth-token.spec.ts
+++ src/components/manifold-auth-token/manifold-auth-token.spec.ts
@@ -1,19 +1,40 @@
 import { ManifoldAuthToken } from './manifold-auth-token';
 
 describe('<manifold-auth-token>', () => {
-  it('calls the set auth token on load', () => {
+  describe('when the token is not expired', () => {
     const expiry = new Date();
     expiry.setDate(expiry.getDate() + 1);
     const encodedExpiry = Buffer.from(expiry.toUTCString()).toString('base64');
-    const token = `test.${encodedExpiry}`;
 
-    const provisionButton = new ManifoldAuthToken();
-    provisionButton.setAuthToken = jest.fn();
+    it('calls the set auth token on load', () => {
+      const token = `test.${encodedExpiry}`;
 
-    provisionButton.token = token;
-    provisionButton.componentWillLoad();
+      const provisionButton = new ManifoldAuthToken();
+      provisionButton.setAuthToken = jest.fn();
 
-    expect(provisionButton.setAuthToken).toHaveBeenCalledWith(token);
+      provisionButton.token = token;
+      provisionButton.componentWillLoad();
+
+      expect(provisionButton.setAuthToken).toHaveBeenCalledWith(token);
+    });
+  });
+
+  describe('when the token is expired', () => {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() - 1);
+    const encodedExpiry = Buffer.from(expiry.toUTCString()).toString('base64');
+
+    it('calls the set auth token on load', () => {
+      const token = `test.${encodedExpiry}`;
+
+      const provisionButton = new ManifoldAuthToken();
+      provisionButton.setAuthToken = jest.fn();
+
+      provisionButton.token = token;
+      provisionButton.componentWillLoad();
+
+      expect(provisionButton.setAuthToken).not.toHaveBeenCalled();
+    });
   });
 
   it('calls the set auth token on change', () => {
