4c287e70667ed8f97e261db7ba344084ea1bb5fd
Guillaume St-Pierre <guillaume@manifold.co>
2019-07-22 16:45:18 -0300

    Remove the render prop from the resource product to allow slots

--- src/components/manifold-resource-product/manifold-resource-product.tsx
+++ src/components/manifold-resource-product/manifold-resource-product.tsx
@@ -1,39 +1,38 @@
 import { h, Component, Prop } from '@stencil/core';
 
-import ResourceTunnel, { ResourceState } from '../../data/resource';
+import ResourceTunnel from '../../data/resource';
+import { Gateway } from '../../types/gateway';
 
 @Component({ tag: 'manifold-resource-product' })
 export class ManifoldResourceProduct {
+  @Prop() data?: Gateway.Resource;
+  @Prop() loading: boolean = true;
   @Prop() asCard?: boolean = false;
 
   render() {
-    return (
-      <ResourceTunnel.Consumer>
-        {(state: ResourceState) =>
-          !state.loading && state.data && state.data.product ? (
-            <manifold-service-view
-              name={state.data.product.name}
-              description={state.data.product.tagline}
-              label={state.data.product.label}
-              logo={state.data.product.logo_url}
-              productId={state.data.product.id}
-              asCard={this.asCard}
-            >
-              <manifold-forward-slot slot="cta">
-                <slot name="cta" />
-              </manifold-forward-slot>
-            </manifold-service-view>
-          ) : (
-            // ☠
-            <manifold-service-view
-              loading={true}
-              name="loading..."
-              description="This is a loading product..."
-              asCard={this.asCard}
-            />
-          )
-        }
-      </ResourceTunnel.Consumer>
+    return this.data && this.data.product ? (
+      <manifold-service-view
+        name={this.data.product.name}
+        description={this.data.product.tagline}
+        label={this.data.product.label}
+        logo={this.data.product.logo_url}
+        productId={this.data.product.id}
+        asCard={this.asCard}
+      >
+        <manifold-forward-slot slot="cta">
+          <slot name="cta" />
+        </manifold-forward-slot>
+      </manifold-service-view>
+    ) : (
+      // ☠
+      <manifold-service-view
+        loading={true}
+        name="loading..."
+        description="This is a loading product..."
+        asCard={this.asCard}
+      />
     );
   }
 }
+
+ResourceTunnel.injectProps(ManifoldResourceProduct, ['data', 'loading']);
