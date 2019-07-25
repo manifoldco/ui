9f65874c2beefdceca72d5ea2125354d213563c8
Guillaume St-Pierre <guillaume@manifold.co>
2019-07-25 10:17:07 -0300

    Fix the resource list e2e test by removing it
    This replaces the resource list's flaky e2e test with a new shiny spec test that can actually test things and give us more confidence in the resource list code.

--- src/components/manifold-resource-list/manifold-resource-list.spec.ts
+++ src/components/manifold-resource-list/manifold-resource-list.spec.ts
@@ -1,3 +1,4 @@
+import { newSpecPage } from '@stencil/core/testing';
 import fetchMock from 'fetch-mock';
 
 import { ManifoldResourceList } from './manifold-resource-list';
@@ -5,6 +6,7 @@ import { Marketplace } from '../../types/marketplace';
 import { Provisioning } from '../../types/provisioning';
 import { Resource } from '../../spec/mock/marketplace';
 import { Product } from '../../spec/mock/catalog';
+import { connections } from '../../utils/connections';
 
 const resources: Marketplace.Resource[] = [
   {
@@ -33,20 +35,81 @@ describe('<manifold-resource-list>', () => {
     fetchMock.restore();
   });
 
+  describe('The two slots for the state of the components', () => {
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it('The "no-resources" slot is rendered if no resources are found.', async () => {
+      fetchMock
+        .mock(`${connections.prod.marketplace}/resources/?me`, [])
+        .mock(`${connections.prod.catalog}/products`, [])
+        .mock(`${connections.prod.provisioning}/operations/?is_deleted=false`, []);
+
+      const page = await newSpecPage({
+        components: [ManifoldResourceList],
+        html: `
+          <manifold-resource-list>
+            <div data-test="no-resources" slot="no-resources"></div>
+          </manifold-resource-list>
+        `,
+      });
+
+      // @ts-ignore
+      expect(page.root.querySelector('[data-test="no-resources"]')).toBeTruthy();
+    });
+
+    it('The "loading" state is rendered if still loading.', async () => {
+      fetchMock
+        .mock(`${connections.prod.marketplace}/resources/?me`, {})
+        .mock(`${connections.prod.catalog}/products`, [])
+        .mock(`${connections.prod.provisioning}/operations/?is_deleted=false`, []);
+
+      const page = await newSpecPage({
+        components: [ManifoldResourceList],
+        html: `
+          <manifold-resource-list>
+            <div data-test="loading" slot="loading"></div>
+          </manifold-resource-list>
+        `,
+      });
+
+      // @ts-ignore
+      expect(page.root.querySelector('[data-test="loading"]')).toBeTruthy();
+    });
+
+    it('The resources list are rendered if given.', async () => {
+      fetchMock
+        .mock(`${connections.prod.marketplace}/resources/?me`, resources)
+        .mock(`${connections.prod.catalog}/products`, [Product])
+        .mock(`${connections.prod.provisioning}/operations/?is_deleted=false`, [
+          provisionOperation,
+        ]);
+
+      const page = await newSpecPage({
+        components: [ManifoldResourceList],
+        html: `
+          <manifold-resource-list></manifold-resource-list>
+        `,
+      });
+
+      // TODO: Add a test that will test that the resources are rendered with their various states
+      // to replace the 'processes resources properly' here.
+      // @ts-ignore
+      expect(page.root.shadowRoot.querySelector('.wrapper')).toBeTruthy();
+      // @ts-ignore
+      expect(page.root.shadowRoot.querySelectorAll('manifold-resource-card-view')).toHaveLength(1);
+    });
+  });
+
   it('processes resources properly', async () => {
     fetchMock
-      .mock('/resources/?me', resources)
-      .mock('/products', [Product])
-      .mock('/operations/?is_deleted=false', [provisionOperation]);
+      .mock(`${connections.prod.marketplace}/resources/?me`, resources)
+      .mock(`${connections.prod.catalog}/products`, [Product])
+      .mock(`${connections.prod.provisioning}/operations/?is_deleted=false`, [provisionOperation]);
 
     const resourceList = new ManifoldResourceList();
-    resourceList.connection = {
-      provisioning: '',
-      catalog: '',
-      gateway: '',
-      marketplace: '',
-      billing: '',
-    };
+    resourceList.connection = connections.prod;
 
     await resourceList.fetchResources();
 
