84b15c56c6b5ddb66530440289d9841a60ca127e
Chloe Edeal <caedeal@gmail.com>
2019-07-25 10:47:49 -0300

    Use shadowcat for oauth  (#258)
    * WIP: added shadowcat to the auth-token component and attempted to set the iframe message as the token
    
    * get that token workin'
    
    * cleanup
    
    * refactor event listener
    
    * Raise a DOM event when the token validity changes
    
    * Set the received token in local storage
    
    * Unconditionally emit the new token
    
    * Use token expiry information
    
    * repair after rebase
    
    * Add expired variant to token tests
    
    * Add a fallback for our demo apps
    
    * Adding token explanation to Storybook internal docs
    
    * Refining notes about expired token
    
    * Fixing typo
    
    * Switch to unix time for expiry
    
    * Upgrade shadowcat for unix timestamp change
    
    * Remove unneeded import

--- src/components/manifold-connection/manifold-connection.tsx
+++ src/components/manifold-connection/manifold-connection.tsx
@@ -2,24 +2,46 @@ import { h, Component, Prop, State } from '@stencil/core';
 
 import Tunnel from '../../data/connection';
 import { connections } from '../../utils/connections';
+import { isExpired } from '../../utils/auth';
+
+const TOKEN_KEY = 'manifold_api_token';
+
+function getDefaultToken() {
+  const token = localStorage.getItem(TOKEN_KEY);
+  if (token && !isExpired(token)) {
+    return token;
+  }
+
+  return undefined;
+}
 
 @Component({ tag: 'manifold-connection' })
 export class ManiTunnel {
   /** _(optional)_ Specify `env="stage"` for staging */
   @Prop() env: 'stage' | 'prod' = 'prod';
 
-  @State() authToken?: string = localStorage.getItem('manifold_api_token') || undefined;
+  @State() authToken?: string = getDefaultToken();
 
   setAuthToken = (token: string) => {
     this.authToken = token;
+    localStorage.setItem(TOKEN_KEY, token);
   };
+
+  get accessToken() {
+    if (this.authToken) {
+      const [token] = this.authToken.split('.');
+      return token;
+    }
+
+    return undefined;
+  }
 
   render() {
     return (
       <Tunnel.Provider
         state={{
           connection: connections[this.env],
-          authToken: this.authToken,
+          authToken: this.accessToken,
           setAuthToken: this.setAuthToken,
         }}
       >
