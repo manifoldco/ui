251ffcd98a771ae0cdd8e796f5d8c4601ff18fe0
David Leger <dleger@manifold.co>
2019-07-25 14:58:30 -0300

    add graphql fetch to manifold connection (#277)
    * update deps
    
    * added graphqlFetch to manifold-connection
    
    * update changelog
    
    * fix graphql fetch stuff a bit
    
    * figuring out auth things
    
    * remove alpha-sapphire theme
    
    * remove alpha-sapphire theme
    
    * added to changelog
    
    * separate resource logo from product logo component
    
    * fix resource-logo docs
    
    * cleanup
    
    * update types
    
    * updated changelogs
    
    * add connection to logo example

--- src/components/manifold-data-product-logo/manifold-data-product-logo.tsx
+++ src/components/manifold-data-product-logo/manifold-data-product-logo.tsx
@@ -1,12 +1,8 @@
-import { h, Component, Prop, State, Element, Watch } from '@stencil/core';
+import { h, Component, Prop, State, Watch } from '@stencil/core';
 import { gql } from '@manifoldco/gql-zero';
-
-import graphqlFetch from '../../utils/graphqlFetch';
-import { Catalog } from '../../types/catalog';
-import { Gateway } from '../../types/gateway';
+import { GraphqlResponseBody, GraphqlRequestBody } from '../../utils/graphqlFetch';
 import { Product } from '../../types/graphql';
 import Tunnel from '../../data/connection';
-import { withAuth } from '../../utils/auth';
 import { Connection, connections } from '../../utils/connections';
 
 const query = gql`
@@ -20,77 +16,52 @@ const query = gql`
 
 @Component({ tag: 'manifold-data-product-logo' })
 export class ManifoldDataProductLogo {
-  @Element() el: HTMLElement;
   /** _(optional)_ `alt` attribute */
   @Prop() alt?: string;
   /** _(hidden)_ Passed by `<manifold-connection>` */
   @Prop() connection?: Connection = connections.prod; // Provided by manifold-connection
   /** _(hidden)_ Passed by `<manifold-connection>` */
   @Prop() authToken?: string;
+  /** _(hidden)_ Passed by `<manifold-connection>` */
+  @Prop() graphqlFetch?: <T>(body: GraphqlRequestBody) => GraphqlResponseBody<T>;
   /** URL-friendly slug (e.g. `"jawsdb-mysql"`) */
   @Prop() productLabel?: string;
-  /** Look up product name from resource */
+  /** _(Depricated)_ Look up product logo from resource */
   @Prop() resourceLabel?: string;
   @State() product?: Product;
+  @Watch('authToken') refetch() {
+    this.fetchProduct(this.productLabel);
+  }
   @Watch('productLabel') productChange(newProduct: string) {
     this.fetchProduct(newProduct);
   }
-  @Watch('resourceLabel') resourceChange(newResource: string) {
-    this.fetchResource(newResource);
-  }
 
   componentWillLoad() {
-    if (this.productLabel) {
-      this.fetchProduct(this.productLabel);
-    }
-    if (this.resourceLabel) {
-      this.fetchResource(this.resourceLabel);
-    }
+    this.fetchProduct(this.productLabel);
   }
 
-  fetchProduct = async (productLabel: string) => {
-    if (!this.connection) {
+  fetchProduct = async (productLabel?: string) => {
+    if (!this.graphqlFetch || !this.productLabel) {
       return;
     }
 
     this.product = undefined;
     const variables = { productLabel };
-    const { data, error } = await graphqlFetch({ query, variables });
-    if (error) {
-      console.error(error);
-    }
+    const { data } = await this.graphqlFetch({ query, variables });
+
     this.product = data.product;
   };
 
-  fetchResource = async (resourceLabel: string) => {
-    if (!this.connection) {
-      return;
+  render() {
+    if (this.resourceLabel) {
+      console.warn('DEPRCATION WARNING: Use `manifold-data-resource-logo` instead.');
+      return <manifold-data-resource-logo resourceLabel={this.resourceLabel} />;
     }
 
-    this.product = undefined;
-    const { catalog, gateway } = this.connection;
-    const response = await fetch(
-      `${gateway}/resources/me/${resourceLabel}`,
-      withAuth(this.authToken)
-    );
-    const resource: Gateway.Resource = await response.json();
-    const productId = resource.product && resource.product.id;
-    const productResp = await fetch(`${catalog}/products/${productId}`, withAuth(this.authToken));
-    const product: Catalog.Product = await productResp.json();
-
-    // NOTE: Temporary util GraphQL supports resources
-    const newProduct = {
-      displayName: product.body.name,
-      logoUrl: product.body.logo_url,
-    };
-    this.product = newProduct as Product;
-  };
-
-  render() {
     return this.product ? (
       <img src={this.product.logoUrl} alt={this.alt || this.product.displayName} />
     ) : null;
   }
 }
 
-Tunnel.injectProps(ManifoldDataProductLogo, ['connection', 'authToken']);
+Tunnel.injectProps(ManifoldDataProductLogo, ['connection', 'authToken', 'graphqlFetch']);
