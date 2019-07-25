a99bd37fca5d32173227ea9e1816e31c6b6ebd29
Guillaume St-Pierre <guillaume@manifold.co>
2019-07-25 15:02:32 -0300

    Fix the shadowcat inclusion in app.ts
    This makes sure that shadowcat is truly loaded in the bundle fo UI and that the token will be set on the web component.

--- src/components/manifold-auth-token/manifold-auth-token.tsx
+++ src/components/manifold-auth-token/manifold-auth-token.tsx
@@ -24,13 +24,14 @@ export class ManifoldAuthToken {
     }
   }
 
-  setInternalToken(e: CustomEvent) {
+  setInternalToken = (e: CustomEvent) => {
     const payload = e.detail as AuthToken;
     if (!payload.error && payload.expiry) {
       const encodedExpiry = Buffer.from(payload.expiry.toString()).toString('base64');
+      // This does not trigger the watch
       this.token = `${payload.token}.${encodedExpiry}`;
     }
-  }
+  };
 
   render() {
     return (
