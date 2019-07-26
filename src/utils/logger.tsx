d6885aee1be2d937dcc54a409feb28b44d078e8b
Drew Powers <drew@pow.rs>
2019-07-26 10:00:26 -0300

    Add error reporting, error display, tests

---
+++ src/utils/logger.tsx
@@ -0,0 +1,32 @@
+import { h } from '@stencil/core';
+
+interface ErrorDetail {
+  error: string;
+}
+
+/* eslint-disable no-param-reassign */
+
+export default function logger<T>() {
+  return function loggerDecorator(
+    _target: T,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function render(...args: T[]) {
+      try {
+        const result = originalMethod.apply(this, args); // call render()
+        return result;
+      } catch (e) {
+        console.error(e); // report error (Rollbar, Datadog, etc.)
+        const detail: ErrorDetail = { error: e.message };
+        const evt = new CustomEvent('manifold-error', { detail }); // custom event
+        window.dispatchEvent(evt);
+        return <manifold-toast alert-type="error">{e.message}</manifold-toast>;
+      }
+    };
+
+    return descriptor;
+  };
+}
