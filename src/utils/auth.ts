3c1af97ba035f486b81056562efa7e83c2230aeb
David Leger <dleger@manifold.co>
2019-07-26 11:18:12 -0300

    add tests

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -26,7 +26,10 @@ export function withAuth(authToken?: string, options?: RequestInit): RequestInit
 export function isExpired(token: string) {
   try {
     const expiry = token.split('.').pop() || '';
-    const decodedExpiry = parseInt(Buffer.from(expiry, 'base64').toString(), 10);
+    const decodedExpiry = parseInt(expiry, 10);
+    if (Number.isNaN(decodedExpiry)) {
+      throw new Error('Expiry not a number.');
+    }
 
     const d = new Date(decodedExpiry * 1000);
     const now = new Date();
