0c8edb8d2c870876b2e3ffd306beb1802025fb87
Guillaume St-Pierre <guillaume@manifold.co>
2019-07-31 13:23:02 -0400

    Fix the active plan data injection for happo

--- src/components/manifold-active-plan/manifold-active-plan-happo.ts
+++ src/components/manifold-active-plan/manifold-active-plan-happo.ts
@@ -1,11 +1,13 @@
 import product from '../../spec/mock/jawsdb/product.json';
 import plans from '../../spec/mock/jawsdb/plans.json';
+import regions from '../../spec/mock/jawsdb/regions.json';
 import fromJSON from '../../spec/mock/fromJSON';
 
 export const jawsDB = () => {
   const plan = document.createElement('manifold-active-plan');
   plan.plans = fromJSON(plans);
   plan.product = fromJSON(product);
+  plan.regions = fromJSON(regions);
 
   document.body.appendChild(plan);
 
@@ -16,6 +18,7 @@ export const planError = () => {
   const plan = document.createElement('manifold-active-plan');
   plan.plans = fromJSON(plans);
   plan.product = fromJSON(product);
+  plan.regions = fromJSON(regions);
   plan.selectedResource = {
     get region() {
       throw new Error('oops'); // strangely-specific render() error for this component
