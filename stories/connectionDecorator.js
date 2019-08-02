ebe2f112de85d33014ea70f67b618ea8725226a0
Guillaume St-Pierre <guillaume@manifold.co>
2019-08-02 10:22:35 -0400

    Fixed typo

--- stories/connectionDecorator.js
+++ stories/connectionDecorator.js
@@ -1,5 +1,5 @@
 // Creates a fake token provider and manifold connection that adds the api token from localstorage
-// with an expiriy date a week from when it is created. 6.04e8 is a week in miliseconds.
+// with an expiriy date a week from when it is created. 6.04e8 is a week in milliseconds.
 export const manifoldConnectionDecorator = storyFn => `
   <manifold-connection>
     <manifold-auth-token token="${localStorage.manifold_api_token}|${new Date(
