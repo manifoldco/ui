c54bc961dc7328dce4a44adff784cbc769e8eeee
Guillaume St-Pierre <guillaume@manifold.co>
2019-08-12 11:17:00 -0400

    Add happo tests for the custom button colors

--- src/components/manifold-credentials-view/manifold-credentials-view-happo.ts
+++ src/components/manifold-credentials-view/manifold-credentials-view-happo.ts
@@ -1,5 +1,6 @@
 import credentials from '../../spec/mock/cms-stage/credentials.json';
 import fromJSON from '../../spec/mock/fromJSON';
+import { buttonColors } from '../manifold-button/manifold-button';
 
 export const credsHidden = () => {
   const creds = document.createElement('manifold-credentials-view');
@@ -27,4 +28,43 @@ export const resourceLoading = () => {
   document.body.appendChild(creds);
 
   return creds.componentOnReady();
+};
+
+export const credsHiddenWithCustomColor = () => {
+  const container = document.createElement('div');
+  const promises = Promise.all(
+    ['black', 'gray', 'orange', 'pink', 'white'].map(color => {
+      const creds = document.createElement('manifold-credentials-view');
+      creds.resourceLabel = 'test';
+      creds.showButtonColor = color as buttonColors;
+
+      container.appendChild(creds);
+
+      return creds.componentOnReady();
+    })
+  );
+
+  document.body.appendChild(container);
+
+  return promises;
+};
+
+export const credsShownWithCustomColor = () => {
+  const container = document.createElement('div');
+  const promises = Promise.all(
+    ['black', 'gray', 'orange', 'pink', 'white'].map(color => {
+      const creds = document.createElement('manifold-credentials-view');
+      creds.resourceLabel = 'test';
+      creds.credentials = fromJSON(credentials);
+      creds.hideButtonColor = color as buttonColors;
+
+      container.appendChild(creds);
+
+      return creds.componentOnReady();
+    })
+  );
+
+  document.body.appendChild(container);
+
+  return promises;
 };
