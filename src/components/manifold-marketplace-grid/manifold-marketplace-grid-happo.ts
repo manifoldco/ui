63016d4aacdc4f6e745c8ded7b3f7bb7f7dc9691
Drew Powers <drew@pow.rs>
2019-08-12 23:46:35 -0600

    Fix Happo tests

--- src/components/manifold-marketplace-grid/manifold-marketplace-grid-happo.ts
+++ src/components/manifold-marketplace-grid/manifold-marketplace-grid-happo.ts
@@ -1,8 +1,11 @@
+import skeletonProducts from '../../data/marketplace';
 import products from '../../spec/mock/products.json';
 import fromJSON from '../../spec/mock/fromJSON';
 
 export const skeleton = () => {
   const grid = document.createElement('manifold-marketplace-grid');
+  grid.services = fromJSON(skeletonProducts);
+  grid.skeleton = true;
 
   document.body.appendChild(grid);
 
