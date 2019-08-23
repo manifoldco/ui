49a55a182950d5e06195a8a099c265d8039a1fc5
Sam Slotsky <sam@manifold.co>
2019-08-23 10:57:21 -0500

    Add unit test for connection subscription

---
+++ src/state/connection.spec.ts
@@ -0,0 +1,23 @@
+import { ConnectionState } from './connection';
+
+describe('connection state', () => {
+  it('notifies subscribers of auth token changes', () => {
+    const state = new ConnectionState();
+    state.authToken = 'old-token';
+    const subscriber = jest.fn();
+    state.subscribe(subscriber);
+    state.setAuthToken('new-token');
+    expect(subscriber).toHaveBeenCalledWith('old-token', 'new-token');
+  });
+
+  it('will not notify after unsubscribing', () => {
+    const state = new ConnectionState();
+    state.authToken = 'old-token';
+    const subscription = jest.fn();
+    const unsubscribe = state.subscribe(subscription);
+    state.setAuthToken('new-token');
+    unsubscribe();
+    state.setAuthToken('old-token');
+    expect(subscription.mock.calls).toHaveLength(1);
+  });
+});
