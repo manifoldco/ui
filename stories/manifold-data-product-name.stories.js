bd6030cc911a31e4d992256402540a9ebe8c0029
Drew Powers <drew@pow.rs>
2019-08-29 16:16:44 -0600

    Add Storybook Knobs

--- stories/manifold-data-product-name.stories.js
+++ stories/manifold-data-product-name.stories.js
@@ -1,17 +1,18 @@
 import { storiesOf } from '@storybook/html';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import markdown from '../docs/docs/data/manifold-data-product-name.md';
 import { manifoldConnectionDecorator } from './connectionDecorator';
 
 storiesOf('Product Name', module)
+  .addDecorator(withKnobs)
   .addParameters({ readme: { sidebar: markdown } })
   .addDecorator(manifoldConnectionDecorator)
-  .add(
-    'from product label',
-    () =>
-      '<manifold-data-product-name product-label="jawsdb-postgres"></manifold-data-product-name>'
-  )
-  .add(
-    'from resource',
-    () => '<manifold-data-product-name resource-label="my-resource"></manifold-data-product-name>'
-  );
+  .add('from product label', () => {
+    const productLabel = text('product-label', 'jawsdb-postgres');
+    return `<manifold-data-product-name product-label="${productLabel}"></manifold-data-product-name>`;
+  })
+  .add('from resource', () => {
+    const resourceLabel = text('resource-label (requires manifold_api_key)', 'config');
+    return `<manifold-data-product-name resource-label="${resourceLabel}"></manifold-data-product-name>`;
+  });
