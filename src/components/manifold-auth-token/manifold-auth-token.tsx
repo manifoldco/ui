c462b1af8d60dc3d590e832fd984e46631f04eb7
Drew Powers <dangodev@users.noreply.github.com>
2019-09-04 20:51:40 -0700

    Update shadowcat (#477)
    * Update shadowcat
    
    * Update CHANGELOG
    
    * Update shadowcat
    
    * Restore subscribe function
    
    * Make all props optional
    
    * Update shadowcat to 0.1.7

--- src/components/manifold-auth-token/manifold-auth-token.tsx
+++ src/components/manifold-auth-token/manifold-auth-token.tsx
@@ -1,4 +1,4 @@
-import { h, Component, Prop, Watch, Event, EventEmitter } from '@stencil/core';
+import { h, Component, Prop, State, Watch, Event, EventEmitter } from '@stencil/core';
 
 import { AuthToken } from '@manifoldco/shadowcat';
 import Tunnel from '../../data/connection';
@@ -9,12 +9,12 @@ import { Subscriber } from '../../state/connection';
 @Component({ tag: 'manifold-auth-token' })
 export class ManifoldAuthToken {
   /** _(hidden)_ Passed by `<manifold-connection>` */
-  @Prop() setAuthToken: (s: string) => void = () => {};
+  @Prop() setAuthToken?: (s: string) => void = () => {};
   /** _(hidden)_ Passed by `<manifold-connection>` */
-  @Prop() subscribe: (s: Subscriber) => () => void = () => () => {};
+  @Prop() subscribe?: (s: Subscriber) => () => void = () => () => {};
   /* Authorisation header token that can be used to authenticate the user in manifold */
   @Prop() token?: string;
-  @Prop() oauthUrl?: string = 'https://login.manifold.co/signin/oauth/web';
+  @State() tick?: string;
   @Event({ eventName: 'manifold-token-receive', bubbles: true })
   manifoldOauthTokenChange: EventEmitter<{ token: string }>;
 
@@ -28,10 +28,9 @@ export class ManifoldAuthToken {
     this.setExternalToken(this.token);
     if (this.subscribe) {
       this.unsubscribe = this.subscribe((oldToken?: string, newToken?: string) => {
-        if (oldToken && !newToken && this.oauthUrl) {
-          const url = new URL(this.oauthUrl, window.location.href);
-          url.searchParams.set('ts', new Date().getTime().toString());
-          this.oauthUrl = url.href;
+        if (oldToken && !newToken) {
+          // changing this to any new string will cause a token refresh. getTime() does that wonderfully.
+          this.tick = new Date().getTime().toString();
         }
       });
     }
@@ -43,7 +42,7 @@ export class ManifoldAuthToken {
 
   setExternalToken(token?: string) {
     if (token) {
-      if (!isExpired(token)) {
+      if (!isExpired(token) && this.setAuthToken) {
         this.setAuthToken(token);
       }
     }
@@ -53,16 +52,16 @@ export class ManifoldAuthToken {
     const payload = e.detail as AuthToken;
     if (!payload.error && payload.expiry) {
       const formattedToken = `${payload.token}|${payload.expiry}`;
-      this.setAuthToken(formattedToken);
+      if (this.setAuthToken) {
+        this.setAuthToken(formattedToken);
+      }
       this.manifoldOauthTokenChange.emit({ token: formattedToken });
     }
   };
 
   @logger()
   render() {
-    return (
-      <manifold-oauth onReceiveManifoldToken={this.setInternalToken} oauthUrl={this.oauthUrl} />
-    );
+    return <manifold-oauth tick={this.tick} onReceiveManifoldToken={this.setInternalToken} />;
   }
 }
 
