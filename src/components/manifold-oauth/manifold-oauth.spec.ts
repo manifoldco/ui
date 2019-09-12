c20bd4ba4f3a1b6b3bfbab3ad9149363ba6dc8cf
Drew Powers <drew@pow.rs>
2019-09-12 14:19:47 -0400

    Add test for receive token event

--- src/components/manifold-oauth/manifold-oauth.spec.ts
+++ src/components/manifold-oauth/manifold-oauth.spec.ts
@@ -2,7 +2,7 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ManifoldOauth } from './manifold-oauth';
 
 describe('<manifold-oauth>', () => {
-  describe('v0 API', () => {
+  describe('v0 props', () => {
     // Testing implementation details isn’t preferred, but E2E testing with
     // page.on('request') proved to be too difficult with Stencil.
     it('[tick]: when changed it resets iframe', async () => {
@@ -22,6 +22,46 @@ describe('<manifold-oauth>', () => {
       rootInstance.tick = new Date().getTime();
       await page.waitForChanges();
       expect(mockRefresh).toHaveBeenCalled();
+    });
+  });
+
+  describe('v0 events', () => {
+    it('receiveManifoldToken', async () => {
+      const data = {
+        access_token: 'secret-token',
+        duration: 1,
+        expiry: 1,
+      };
+
+      const mock = jest.fn();
+      const page = await newSpecPage({
+        components: [ManifoldOauth],
+        html: '<manifold-oauth></manifold-oauth>',
+      });
+      await page.waitForChanges();
+      const component = page.root;
+      if (!component) {
+        throw new Error('<manifold-oauth> not in document');
+      }
+      component.addEventListener('receiveManifoldToken', mock);
+
+      // send bad origin, expect token to not be received
+      page.rootInstance.tokenListener({ origin: 'http://badorigin.com', data });
+      expect(mock).not.toHaveBeenCalled();
+
+      // send correct origin, expect token to be received
+      const goodEvent = { origin: 'https://login.manifold.co', data };
+      page.rootInstance.tokenListener(goodEvent);
+      expect(mock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          detail: {
+            duration: data.duration,
+            error: undefined,
+            expiry: data.expiry,
+            token: data.access_token,
+          },
+        })
+      );
     });
   });
 });
