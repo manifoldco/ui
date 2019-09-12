f299badb0071cbc24a8dce44c1c05e26cef107c4
Drew Powers <drew@pow.rs>
2019-09-12 10:18:15 -0400

    Remove type export

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,7 +1,3 @@
-// Expose library interfaces via npm
-// -> https://github.com/ionic-team/ionic/blob/master/core/src/interface.d.ts#L3
-export * from '../components'; // eslint-disable-line import/no-cycle
-
 export interface AuthError {
   code: number;
   message: string;
