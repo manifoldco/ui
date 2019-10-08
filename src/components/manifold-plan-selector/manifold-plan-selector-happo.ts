2272c13e8834f45c1e2b28b2a5c65a0799589111
Drew Powers <drew@pow.rs>
2019-10-08 17:28:32 -0600

    Add more VRT examples

--- src/components/manifold-plan-selector/manifold-plan-selector-happo.ts
+++ src/components/manifold-plan-selector/manifold-plan-selector-happo.ts
@@ -1,4 +1,11 @@
-import product from '../../spec/mock/jawsdb/product.json';
+import productBlitline from '../../spec/mock/blitline/product.json';
+import productGenericTagging from '../../spec/mock/generic-tagging/product.json';
+import productJawsDB from '../../spec/mock/jawsdb/product.json';
+import productLogdna from '../../spec/mock/logdna/product.json';
+import productPrefab from '../../spec/mock/prefab/product.json';
+import productMailgun from '../../spec/mock/mailgun/product.json';
+import productZerosix from '../../spec/mock/zerosix/product.json';
+import productZiggeo from '../../spec/mock/ziggeo/product.json';
 import connection from '../../state/connection';
 import { RestFetch } from '../../utils/restFetch';
 import { GraphqlFetch } from '../../utils/graphqlFetch';
@@ -8,7 +15,7 @@ export const skeleton = async () => {
   document.body.appendChild(conn);
 
   const selector = document.createElement('manifold-plan-selector');
-  selector.productLabel = product.body.label;
+  selector.productLabel = productJawsDB.body.label;
 
   const mockFetch = (async (...args) => {
     // Simulate a delay so we see the skeletons
@@ -30,12 +37,103 @@ export const skeleton = async () => {
   await selector.componentOnReady();
 };
 
+export const blitline = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productBlitline.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const genericTagging = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productGenericTagging.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
 export const jawsDB = async () => {
   const conn = document.createElement('manifold-connection');
   document.body.appendChild(conn);
 
   const selector = document.createElement('manifold-plan-selector');
-  selector.productLabel = product.body.label;
+  selector.productLabel = productJawsDB.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const logdna = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productLogdna.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const prefab = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productPrefab.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const mailgun = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productMailgun.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const zerosix = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productZerosix.body.label;
+  selector.hideUntilReady = true;
+
+  document.body.appendChild(selector);
+
+  await selector.componentOnReady();
+};
+
+export const ziggeo = async () => {
+  const conn = document.createElement('manifold-connection');
+  document.body.appendChild(conn);
+
+  const selector = document.createElement('manifold-plan-selector');
+  selector.productLabel = productZiggeo.body.label;
   selector.hideUntilReady = true;
 
   document.body.appendChild(selector);
@@ -48,7 +146,7 @@ export const delayedJawsDB = async () => {
   document.body.appendChild(conn);
 
   const selector = document.createElement('manifold-plan-selector');
-  selector.productLabel = product.body.label;
+  selector.productLabel = productJawsDB.body.label;
   selector.hideUntilReady = true;
 
   const mockFetch = (async (...args) => {
@@ -69,13 +167,13 @@ export const planError = async () => {
   document.body.appendChild(conn);
 
   const selector = document.createElement('manifold-plan-selector');
-  selector.productLabel = product.body.label;
+  selector.productLabel = productJawsDB.body.label;
 
   const mockFetch = (async args => {
-    if (args.endpoint === `/plans/?product_id=${product.id}`) {
+    if (args.endpoint === `/plans/?product_id=${productJawsDB.id}`) {
       return [
         {
-          id: product.id,
+          id: productJawsDB.id,
           get body() {
             throw new Error('oops');
           },
