3feed3efe3234facfcf3683e0a0ccf5abfa7eeac
Guillaume St-Pierre <guillaume@manifold.co>
2019-10-08 17:02:25 -0400

    Convert the manifold resource sso component to GraphQL
    Work is related to manifoldco/engineering#9192
    
    Converted the container wrapper for the resource rename component to GraphQL.
    
    Test with Storybook.

---
+++ src/components/manifold-resource-sso/manifold-resource-sso.spec.ts
@@ -0,0 +1,51 @@
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
+
+import { Resource } from '../../types/graphql';
+import { resource } from '../../spec/mock/graphql';
+import { ManifoldResourceSso } from './manifold-resource-sso';
+import { ManifoldDataSsoButton } from '../manifold-data-sso-button/manifold-data-sso-button';
+
+describe('<manifold-resource-sso>', () => {
+  let page: SpecPage;
+  let element: HTMLManifoldResourceSsoElement;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [ManifoldResourceSso, ManifoldDataSsoButton],
+      html: `<div></div>`,
+    });
+    element = page.doc.createElement('manifold-resource-sso');
+  });
+
+  it('Renders a skeleton if loading', async () => {
+    element.loading = true;
+    const root = page.root as HTMLElement;
+    root.appendChild(element);
+
+    await page.waitForChanges();
+
+    const button = element.querySelector('manifold-data-sso-button');
+    expect(button).toBeDefined();
+
+    if (button) {
+      expect(button.loading).toBeTruthy();
+    }
+  });
+
+  it('Renders if not loading', async () => {
+    element.loading = false;
+    element.gqlData = resource as Resource;
+    const root = page.root as HTMLElement;
+    root.appendChild(element);
+
+    await page.waitForChanges();
+
+    const button = element.querySelector('manifold-data-sso-button');
+    expect(button).toBeDefined();
+
+    if (button) {
+      expect(button.loading).toBeFalsy();
+      expect(button.resourceId).toEqual(resource.id);
+      expect(button.resourceLabel).toEqual(resource.label);
+    }
+  });
+});
