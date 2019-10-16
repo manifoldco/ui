da0fa200cdff42827640ca02a2e51b0a3f4b056b
Sam Slotsky <sam@manifold.co>
2019-10-16 10:47:10 -0500

    TestCafe - single test with GitHub action

---
+++ e2e/catalog-test.ts
@@ -0,0 +1,22 @@
+import { Selector } from 'testcafe';
+
+fixture`marketplace`.page`${process.env.DEPLOY_URL}/?path=/story/catalog--marketplace`;
+
+test('filtering', async t => {
+  const iframe = Selector('iframe#storybook-preview-iframe');
+  const filter = Selector(() =>
+    document.querySelector('manifold-marketplace-grid').shadowRoot.querySelector('input.search')
+  );
+  const title = Selector(() =>
+    document
+      .querySelector('manifold-marketplace-grid')
+      .shadowRoot.querySelector('manifold-service-card-view')
+      .shadowRoot.querySelector('h3.name')
+  );
+
+  await t
+    .switchToIframe(iframe)
+    .typeText(filter, 'auth', { paste: true })
+    .expect(title.innerText)
+    .eql('OAuth.io');
+});
