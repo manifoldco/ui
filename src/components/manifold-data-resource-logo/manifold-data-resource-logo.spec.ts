e1005379ebb6e94891f2c7c6607f272096be4228
terrysmithdc <terry@designcollider.com>
2019-10-16 11:33:19 -0300

    Converted spec tests to graphql

--- src/components/manifold-data-resource-logo/manifold-data-resource-logo.spec.ts
+++ src/components/manifold-data-resource-logo/manifold-data-resource-logo.spec.ts
@@ -1,24 +1,24 @@
-import fetchMock from 'fetch-mock';
+import fetchMock, { MockResponseObject } from 'fetch-mock';
 import { newSpecPage } from '@stencil/core/testing';
 import { ManifoldDataResourceLogo } from './manifold-data-resource-logo';
-import { Resource } from '../../spec/mock/marketplace';
-import { Product } from '../../spec/mock/catalog';
-import { connections } from '../../utils/connections';
-import { createRestFetch } from '../../utils/restFetch';
+import { createGraphqlFetch, GraphqlError } from '../../utils/graphqlFetch';
+import { resource } from '../../spec/mock/graphql';
+
+const graphqlEndpoint = 'http://test.com/graphql';
 
 async function setup(resourceLabel: string) {
   const page = await newSpecPage({
     components: [ManifoldDataResourceLogo],
     html: '<div></div>',
   });
-
   const component = page.doc.createElement('manifold-data-resource-logo');
-  component.resourceLabel = resourceLabel;
-  component.restFetch = createRestFetch({
+  component.graphqlFetch = createGraphqlFetch({
+    endpoint: () => graphqlEndpoint,
     getAuthToken: jest.fn(() => '1234'),
     wait: () => 10,
     setAuthToken: jest.fn(),
   });
+  component.resourceLabel = resourceLabel;
 
   const root = page.root as HTMLDivElement;
   root.appendChild(component);
@@ -28,47 +28,46 @@ async function setup(resourceLabel: string) {
 }
 
 describe('<manifold-data-resource-logo>', () => {
+  beforeEach(async () => {
+    fetchMock.mock(graphqlEndpoint, (_, req) => {
+      const body = (req.body && req.body.toString()) || '';
+      const { variables } = JSON.parse(body);
+      const errors: GraphqlError[] = [{ message: 'something went wrong' }];
+      if (body.includes('query RESOURCE')) {
+        // return new label in response
+        const newResource = {
+          ...resource,
+          id: variables.resourceId || resource.id,
+        };
+        return body.includes('error') ? { data: null, errors } : { data: { data: newResource } };
+      }
+      return { data: null, errors };
+    });
+  });
+
   afterEach(() => {
     fetchMock.restore();
   });
 
   it('fetches resource on load', async () => {
     const resourceLabel = 'my-resource';
-    fetchMock.mock(`${connections.prod.marketplace}/resources/?me&label=${resourceLabel}`, [
-      Resource,
-    ]);
-    fetchMock.mock(`${connections.prod.catalog}/products/${Resource.body.product_id}`, Product);
-
     await setup(resourceLabel);
-
-    expect(
-      fetchMock.called(`${connections.prod.marketplace}/resources/?me&label=${resourceLabel}`)
-    ).toBe(true);
-    expect(
-      fetchMock.called(`${connections.prod.catalog}/products/${Resource.body.product_id}`)
-    ).toBe(true);
+    expect(fetchMock.called(`begin:${graphqlEndpoint}`)).toBe(true);
   });
 
   it('fetches resource on change', async () => {
     const resourceLabel = 'my-resource';
-    fetchMock.once('*', []);
-
     const { page, component } = await setup(resourceLabel);
 
     const newResourceLabel = 'new-resource';
-    fetchMock.mock(`${connections.prod.marketplace}/resources/?me&label=${newResourceLabel}`, [
-      Resource,
-    ]);
-    fetchMock.mock(`${connections.prod.catalog}/products/${Resource.body.product_id}`, Product);
 
     component.resourceLabel = newResourceLabel;
     await page.waitForChanges();
 
-    expect(
-      fetchMock.called(`${connections.prod.marketplace}/resources/?me&label=${newResourceLabel}`)
-    ).toBe(true);
-    expect(
-      fetchMock.called(`${connections.prod.catalog}/products/${Resource.body.product_id}`)
-    ).toBe(true);
+    const req = fetchMock.lastOptions(`begin:${graphqlEndpoint}`) as MockResponseObject;
+    const body = (req && req.body && req.body.toString()) || '';
+    const { variables } = JSON.parse(body);
+
+    expect(variables.resourceLabel).toMatch(newResourceLabel);
   });
 });
