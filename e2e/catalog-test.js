af6b39e4aec434bc77391545ef1ab48aff506bcd
Sam Slotsky <sslotsky@users.noreply.github.com>
2019-11-04 09:54:18 -0600

    TestCafe - single test with GitHub action (#637)

---
+++ e2e/catalog-test.js
@@ -0,0 +1,23 @@
+import { Selector } from 'testcafe';
+
+// eslint-disable-next-line
+fixture`marketplace`.page`${process.env.DEPLOY_URL}/?path=/story/catalog--marketplace`;
+
+test('filtering', async t => {
+  const iframe = Selector('iframe#storybook-preview-iframe');
+  const filter = Selector(() =>
+    document.querySelector('manifold-marketplace-grid').shadowRoot.querySelector('input.search')
+  );
+  const title = Selector(() =>
+    document
+      .querySelector('manifold-marketplace-grid')
+      .shadowRoot.querySelector('manifold-service-card-view')
+      .shadowRoot.querySelector('h3.name')
+  );
+
+  await t
+    .switchToIframe(iframe)
+    .typeText(filter, 'auth', { paste: true })
+    .expect(title.innerText)
+    .eql('OAuth.io');
+});
